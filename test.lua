25e207ba03f76ea76a2d90434ac59ae2a01d506d
officialRoxey <notaoeditskel@gmail.com>
2025-06-01 19:28:25 +0100

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,1 +1,19 @@
-print(string.char(104,101,108,108,111,32,119,111,114,108,100))
+-- Anti-debug measures
+local JLZDDAMn = function()
+    local jvQtDzjm = debug or {}
+    if gpjLPcIi.getinfo then
+        error("Debug detected")
+    end
+end
+pcall(xpEVTdkQ)
+
+local KAzuTDbk = function()
+    if math.random() > 0.5 then local nJPUxFHm = nil end
+    local PscbHmsD = function()
+        print(string.reverse("dlrow wolleh"))
+    end
+    local oLCpWxhT = math.random(1, 1000)
+    return PscbHmsD()
+end
+if math.random() > 0.5 then local CnMowiOQ = nil end
+KAzuTDbk()
