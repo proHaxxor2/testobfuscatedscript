08a619fe867a6d931c2c84e0239b9c7ae3eb1e78
officialRoxey <notaoeditskel@gmail.com>
2025-06-01 19:29:11 +0100

    Create test2.lua

---
+++ test2.lua
@@ -0,0 +1,19 @@
+-- Anti-debug measures
+local JLZDDAMn = function()
+    local jvQtDzjm = debug or {}
+    if gpjLPcIi.getinfo then
+        error("Debug detected")
+    end
+end
+pcall(xpEVTdkQ)
+
+local KAzuTDbk = function()
+    if math.random() > 0.5 then local nJPUxFHm = nil end
+    local PscbHmsD = function()
+        print(string.reverse("dlrow wolleh"))
+    end
+    local oLCpWxhT = math.random(1, 1000)
+    return PscbHmsD()
+end
+if math.random() > 0.5 then local CnMowiOQ = nil end
+KAzuTDbk()
